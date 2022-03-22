77fb68a91b2a72c5276aa211a1a689732e2bfdcd
Sn0wBash <102054270+Sn0wBash@users.noreply.github.com>
2022-03-22 18:53:11 -0500

    Create BDE-Log.ps1

---
+++ BDE-Log.ps1
@@ -0,0 +1,18 @@
+#(Get-BitLockerVolume).encryptionpercentage
+
+$name = hostname
+$path = "c:\temp\" + $name + "-BDE.txt"
+$start = date
+
+Write-output "$name `n" | Out-File -FilePath $path -Append
+Write-Output "started at $start `n" | Out-File -FilePath $path -Append
+Get-BitLockerVolume | Format-List | Out-File -FilePath $path -Append
+
+do {
+    $stats = Get-BitLockerVolume
+    $date = date
+    $info = "`nDrive encryption at " + ($stats).encryptionpercentage + "% - " + $date | Out-File -FilePath $path -Append
+    Start-Sleep -s 300
+} While (($stats).encryptionpercentage -lt 100)
+
+Get-BitLockerVolume | Format-List | Out-File -FilePath $path -Append
